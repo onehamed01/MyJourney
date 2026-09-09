ef596d61faa0009c67bedc5bed831b11ed48307f
onehamed01 <hami.mdv01@Gmail.com>
2026-09-09 22:57:14 +0100

    Add Customer with total balance, add account, check count of accounts with specific amount.

--- SolidPython/OOPProject1_banking.py
+++ SolidPython/OOPProject1_banking.py
@@ -23,6 +23,7 @@ class BankAccount:
         if amount > self._balance or amount <= 0:
             return False
         self._balance -= amount
+        return True
     
     @property
     def is_empty(self):
@@ -30,3 +31,54 @@ class BankAccount:
 
 
 
+class SavingAccount(BankAccount):
+    def __init__(self, owner, balance, minimum_balance):
+        super().__init__(owner, balance)
+        self._minimum_balance = minimum_balance
+
+    @property
+    def minimum_balance(self):
+        return self._minimum_balance
+
+    def withdraw(self, amount):
+        if self._minimum_balance > self._balance - amount:
+            return False
+        return super().withdraw(amount)
+
+class Customer:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.collection_bank_account: list[BankAccount] = []
+
+    def add_account(self, account: BankAccount):
+        self.collection_bank_account.append(account)
+
+    def total_balance(self):
+        total_balance = 0
+        for obj in self.collection_bank_account:
+            total_balance += obj.balance
+        return total_balance
+
+    
+    def withdraw_from_all(self, amount):
+        all_accounts = self.collection_bank_account
+        
+        count_accounts = 0
+
+        for obj in all_accounts:
+            if obj.withdraw(amount):
+                count_accounts += 1
+        return count_accounts
+
+
+
+
+hami = Customer('Hami')
+hami_1_bank_account = BankAccount('Hami', 6000)
+hami_1_saving_account = SavingAccount('Hami', 5100, 500)
+hami_2_bank_account = BankAccount('Hami', 5500)
+
+hami.add_account(hami_1_bank_account)
+hami.add_account(hami_1_saving_account)
+hami.add_account(hami_2_bank_account)
+print(hami.withdraw_from_all(5000))
